8ec5edf6621a1e9072ebb956f457417cbf07b333
hznp <1771979508@qq.com>
2025-08-26 23:58:19 +0800

    sdd

---
+++ vue-app/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <div>
+      <Parcel :config="parcelConfig" :mountRootParcel="mountRootParcel" />
+      <router-link to="/foo">foo</router-link>
+      <router-link to="/bar">bar</router-link>
+    </div>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import Parcel from '../node_modules/single-spa-vue/dist/esm/parcel';
+import { mountRootParcel } from 'single-spa'
+
+export default {
+  name: 'App',
+  components: {
+    Parcel
+  },
+  data() {
+    return {
+      parcelConfig: window.System.import('@study/react-nav'),
+      mountParcel: mountRootParcel
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
